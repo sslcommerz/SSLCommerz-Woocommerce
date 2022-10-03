057e89ae776eebb008f1552e15093acfd5cea880
rkbi <rakibul.islam@sslwireless.com>
2022-10-03 16:51:09 +0600

    update IPN handler

--- lib/sslcommerz_ipn.php
+++ lib/sslcommerz_ipn.php
@@ -21,13 +21,22 @@
 		die("SSLCommerz payment gateway is not enabled!");
 	}
 
-	if(isset($_POST['tran_id']) && isset($_POST['val_id']) && (isset($_POST['status']) && $_POST['status'] == "VALID")) 
-	{
-		global $woocommerce;
-		$tran_id = sanitize_text_field($_POST['tran_id']);
-		$val_id = sanitize_text_field($_POST['val_id']);
+	if (empty($_POST['tran_id'])) {
+		die("Invalid IPN Data!");
+	}
 
-        $order = new WC_Order($tran_id);
+	if (empty($_POST['status'])) {
+		die("Invalid IPN Data!");
+	}
+
+
+	global $woocommerce;
+	$tran_id = sanitize_text_field($_POST['tran_id']);
+	$order = new WC_Order($tran_id);
+
+	if(isset($_POST['val_id']) && (isset($_POST['status']) && $_POST['status'] == "VALID")) 
+	{
+		$val_id = sanitize_text_field($_POST['val_id']);
 
 		$requested_url = ($url."?val_id=".$val_id."&store_id=".$store_id."&store_passwd=".$store_passwd."&v=1&format=json");
 		$result = wp_remote_post(
@@ -52,14 +61,14 @@
 			{ 
 				if($result->status=='VALIDATED' || $result->status=='VALID') 
 				{ 
-					if($order->get_status() == 'pending')
+					if($order->get_status() != 'processing')
 					{
 						if($_POST['amount'] != "")
 						{		
 							if($result->risk_level == 0)
 							{
-								$order -> update_status('Processing');
-								$order -> payment_complete();
+								$order->update_status('Processing');
+								$order->payment_complete();
 								$woocommerce->cart->empty_cart();
 								$result_msg =  "<b>IPN Message</b><br>";
 								$result_msg .=  "IPN: Validation success-".$result->status."<br>";
@@ -71,8 +80,8 @@
 							}
 							else
 							{
-								$order -> update_status('on-hold');
-								$order -> payment_complete();
+								$order->update_status('on-hold');
+								$order->payment_complete();
 								$woocommerce->cart->empty_cart();
 								$result_msg =  "<b>IPN Message</b><br>";
 								$result_msg .=  "IPN: Validation success but found Risky-".$result->status."<br>";
@@ -103,11 +112,41 @@
 			{
 				$result_msg= "IPN: Your Paid Amount is Mismatched!";
 			}
-			$order->add_order_note($result_msg);
-			echo $result_msg;
+		}
+	}
+	elseif (in_array($_POST['status'], ["FAILED", "CANCELLED", "UNATTEMPTED", "EXPIRED"]))
+	{
+		if($order->get_status() == 'pending')
+		{
+			if(!empty($_POST['amount']))
+			{
+					if($_POST['status'] == "FAILED") {
+						$new_status = 'failed';
+					} else {
+						$new_status = 'cancelled';
+					}
+
+					$order->update_status($new_status);
+					$result_msg =  "<b>IPN Message</b><br>";
+					$result_msg .=  "IPN: Transaction was - ".$_POST['status']."<br>";
+					$result_msg .=  "IPN: Error - ".$_POST['error']."<br>";
+					$result_msg .=  "IPN: Tran ID-".$tran_id."<br>";
+					$result_msg .=  "IPN: Order status changed to $new_status<br>";
+					$result_msg .=  "IPN: Hit Time - ".date("l d-m-yy h:i:s");
+			}
+			else
+			{
+				$result_msg=  "IPN: Amount can not be empty!";
+			}
+		}	
+		else
+		{
+			$result_msg=  "IPN: Order status already updated!";
 		}
 	}
 	else{
 		die("No IPN Request Received"); 
 	}
-?>
+
+	$order->add_order_note($result_msg);
+	// echo $result_msg;
